61b9cc888920a9950d63a9e25fe3dd4da4d63dc0
Leonty Chudinov <34669449+lchudinov@users.noreply.github.com>
2018-11-08 14:36:42 +0500

    Build
    Signed-off-by: Leonty Chudinov <34669449+lchudinov@users.noreply.github.com>

--- services/popup-manager.service.d.ts
+++ services/popup-manager.service.d.ts
@@ -1,6 +1,4 @@
-
-
-/*
+/*!
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
@@ -9,7 +7,6 @@
   
   Copyright Contributors to the Zowe Project.
 */
-
 import * as Rx from 'rxjs/Rx';
 export interface ErrorReportStruct {
     severity: string;
@@ -38,15 +35,3 @@ export declare class ZluxPopupManagerService {
     getLoggerSeverity(severity: ZluxErrorSeverity): any;
     reportError(severity: ZluxErrorSeverity, title: string, text: string, options?: any): Rx.Observable<any>;
 }
-
-
-/*
-  This program and the accompanying materials are
-  made available under the terms of the Eclipse Public License v2.0 which accompanies
-  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
-  SPDX-License-Identifier: EPL-2.0
-  
-  Copyright Contributors to the Zowe Project.
-*/
-
