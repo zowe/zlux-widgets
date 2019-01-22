ce46210c4bc47e86b3f556d0bd27a6e41bceb3e2
Sean Grady <sgrady@rocketsoftware.com>
2019-01-22 15:28:32 -0500

    Reverting to older webpack to not break
    Signed-off-by: Sean Grady <sgrady@rocketsoftware.com>

--- components/error-report/error-report.component.d.ts
+++ components/error-report/error-report.component.d.ts
@@ -1,15 +1,3 @@
-
-
-/*
-  This program and the accompanying materials are
-  made available under the terms of the Eclipse Public License v2.0 which accompanies
-  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
-  SPDX-License-Identifier: EPL-2.0
-  
-  Copyright Contributors to the Zowe Project.
-*/
-
 import { EventEmitter } from '@angular/core';
 export declare class ZluxErrorReportComponent {
     severity: string;
@@ -21,15 +9,3 @@ export declare class ZluxErrorReportComponent {
 }
 export declare class ZluxErrorReportModule {
 }
-
-
-/*
-  This program and the accompanying materials are
-  made available under the terms of the Eclipse Public License v2.0 which accompanies
-  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
-  SPDX-License-Identifier: EPL-2.0
-  
-  Copyright Contributors to the Zowe Project.
-*/
-
--- services/popup-manager.service.d.ts
+++ services/popup-manager.service.d.ts
@@ -1,15 +1,3 @@
-
-
-/*
-  This program and the accompanying materials are
-  made available under the terms of the Eclipse Public License v2.0 which accompanies
-  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
-  SPDX-License-Identifier: EPL-2.0
-  
-  Copyright Contributors to the Zowe Project.
-*/
-
 import * as Rx from 'rxjs/Rx';
 export interface ErrorReportStruct {
     severity: string;
@@ -38,15 +26,3 @@ export declare class ZluxPopupManagerService {
     getLoggerSeverity(severity: ZluxErrorSeverity): any;
     reportError(severity: ZluxErrorSeverity, title: string, text: string, options?: any): Rx.Observable<any>;
 }
-
-
-/*
-  This program and the accompanying materials are
-  made available under the terms of the Eclipse Public License v2.0 which accompanies
-  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
-  SPDX-License-Identifier: EPL-2.0
-  
-  Copyright Contributors to the Zowe Project.
-*/
-
