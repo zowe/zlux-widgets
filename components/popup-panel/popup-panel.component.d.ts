a847e494c4bfefd1d0ffc41e99c11dae66f68c7f
1000TurquoisePogs <sgrady@rocketsoftware.com>
2019-03-19 16:51:43 -0400

    Fixed license headers
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- components/popup-panel/popup-panel.component.d.ts
+++ components/popup-panel/popup-panel.component.d.ts
@@ -27,3 +27,15 @@ export declare class ZluxPopupPanelComponent implements OnInit, OnDestroy {
 }
 export declare class ZluxPopupPanelModule {
 }
+
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+  
+  SPDX-License-Identifier: EPL-2.0
+  
+  Copyright Contributors to the Zowe Project.
+*/
+
