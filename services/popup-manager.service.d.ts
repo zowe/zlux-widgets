0d1d7af444698664a30356ab52f18dfdc821db0d
1000TurquoisePogs <sgrady@rocketsoftware.com>
2019-03-19 16:53:04 -0400

    Fix missing license header for service dts
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- services/popup-manager.service.d.ts
+++ services/popup-manager.service.d.ts
@@ -1,3 +1,15 @@
+
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+  
+  SPDX-License-Identifier: EPL-2.0
+  
+  Copyright Contributors to the Zowe Project.
+*/
+
 import * as Rx from 'rxjs/Rx';
 export interface ErrorReportStruct {
     severity: string;
@@ -31,3 +43,15 @@ export declare class ZluxPopupManagerService {
     createErrorReport(severity: ZluxErrorSeverity, title: string, text: string, options?: any): ErrorReportStruct;
     reportError(severity: ZluxErrorSeverity, title: string, text: string, options?: any): Rx.Observable<any>;
 }
+
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+  
+  SPDX-License-Identifier: EPL-2.0
+  
+  Copyright Contributors to the Zowe Project.
+*/
+
