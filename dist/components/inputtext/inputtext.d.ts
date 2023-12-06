4795d81221eaa50ec10c8e23baff37931437b3d4
Leanid Astrakou <lastrakou@rocketsoftware.com>
2023-12-06 07:22:31 -0500

    Update to Angular 16
    Signed-off-by: Leanid Astrakou <lastrakou@rocketsoftware.com>

--- dist/components/inputtext/inputtext.d.ts
+++ dist/components/inputtext/inputtext.d.ts
@@ -1,13 +1,3 @@
-/*
-  This program and the accompanying materials are
-  made available under the terms of the Eclipse Public License v2.0 which accompanies
-  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-
-  SPDX-License-Identifier: EPL-2.0
-
-  Copyright Contributors to the Zowe Project.
-*/
-
 import { ElementRef, OnInit } from "@angular/core";
 export declare class ZluxInputText implements OnInit {
     private element;
@@ -17,14 +7,3 @@ export declare class ZluxInputText implements OnInit {
 }
 export declare class ZluxInputTextModule {
 }
-
-
-/*
-  This program and the accompanying materials are
-  made available under the terms of the Eclipse Public License v2.0 which accompanies
-  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-
-  SPDX-License-Identifier: EPL-2.0
-
-  Copyright Contributors to the Zowe Project.
-*/
